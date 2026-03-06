dab1931b7a134093f5f1b1bfbf2e9a55bb9a8a37
Ayyash <137686965+spotlessoutset@users.noreply.github.com>
2026-03-06 15:46:13 +0700

    Add files via upload

---
+++ Praktikum6/soal.py
@@ -0,0 +1,26 @@
+def bubbleSortDescending(data):
+    for batas in range(len(data)-1,0,-1):
+        for i in range(batas):
+            if data[i]<data[i+1]:
+                # Tukar dua data bersebelahan yang urutannya salah
+                temp = data[i]
+                data[i] = data[i+1]
+                data[i+1] = temp
+
+data = [43, 76, 12, 89, 33, 57, 98, 22, 68, 9]
+dict = {}
+
+for i in range(len(data)):
+    dict.update({data[i]:i+1})
+bubbleSortDescending(data)
+print("Nilai-nilai dari 5 kandidat tertinggi adalah",data[:5])
+
+print("kandidat yang lolos adalah kandidat")
+for i in range(5):
+    if i==4:
+        print(" dan ",end="")
+    print("ke-",dict[data[i]],end="")
+    if i != 4:
+        print(",",end="")
+    
+
